51607ddc03368564f9f1eb7e2fcc19e07fd3930c
movingrock <movingrock99@gmail.com>
2024-12-01 13:36:06 +0900

    Initial commit

---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,44 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member){
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * ctrl alt shift T
+     *  리팩토링 단축키
+     */
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
