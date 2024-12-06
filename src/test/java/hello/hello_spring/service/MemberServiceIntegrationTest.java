9a3d2365040d327fed39ac0260147a69ba1a93a6
movingrock <movingrock99@gmail.com>
2024-12-06 19:48:40 +0900

    feat: 20241206 jpa

--- src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-@Transactional
+//@Transactional
 class MemberServiceIntegrationTest {
 
     @Autowired MemberService memberService;
